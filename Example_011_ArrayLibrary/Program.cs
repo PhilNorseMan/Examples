5a440f20cdbffdb7cc4d1a2190b4b06c8dfc030f
Philipp K <philippkargapoloff@gmail.com>
2022-10-19 00:37:41 +0300

    добавлены посянения к некоторым моментам по библиотекам

--- Example_011_ArrayLibrary/Program.cs
+++ Example_011_ArrayLibrary/Program.cs
@@ -1,28 +1,28 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) //void - тип функции, означающий, что функция ничего не возвращает.
 {
-    int length = collection.Length;
+    int length = collection.Length; // задается переменная length, которая соответствует длине collection.
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = new Random().Next(1, 10); //заполнение collection рандомными значениями пошагово в каждый индекс.
         index++; //index = index +1
     }
 }
 
 void PrintArray(int[] col)
 {
-    int count = col.Length;
-    int position = 0;
+    int count = col.Length; // count идентичен length введен ради иного наименования переменной.
+    int position = 0; //position идентичен index введен ради иного наименования перменной.
     while (position < count)
     {
-        Console.WriteLine(col[position]);
+        Console.WriteLine(col[position]); //Вывод в консоль пошагово элементов массива col пока position < cout (то есть пока индекс меньше длины массива)
         position++; //position = position + 1
     }
 }
 
 int IndexOf(int[] collection, int find)
 {
-    int count = collection.Length;
+    int count = collection.Length; //присваиваем переменной count длину массива collection.
     int index = 0;
     int position = -1;
     while (index < count)
