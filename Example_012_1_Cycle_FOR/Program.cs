75d55ddd8526dc08553fb97ee1a5d8b59e76e298
Philipp K <philippkargapoloff@gmail.com>
2022-10-25 01:20:45 +0300

    added methods, ex cycle, cycle in cycle, working with text, sorting in massive

---
+++ Example_012_1_Cycle_FOR/Program.cs
@@ -0,0 +1,17 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for(int i = 0; i < count; i++)
+    //int i = 0 - инициализация счетчика
+    //i < count - проверка условия
+    //i++ - инкримент или увеличение счетчика
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "New Text ");
+Console.WriteLine(res);
+//Цикл for аналогичен циклу while но более удобен в использовании, т.к. все данные вводятся в одном месте.
---
+++ Example_012_4_MassiveSorting_MinMax/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] arr = { 1, 2, 3, 4, 5, 6, 7, 1, 1 };
+
+void PrintArray(int[] arr) // один из способов вывода массива в консоль методом
+{
+    int count = arr.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] arr) //Сортировка элеменотв массива (метод)
+{
+    for(int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPosition = i; //Присваиваем минимальную позицию как текущую позиция
+
+        for(int j = i + 1; j < arr.Length; j++) // Присваиваем шаг в виде джи (выше минимальная позиция выведена как стартовая и)
+        {
+            if(arr[j] < arr[minPosition]) minPosition = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minPosition];
+        arr[minPosition] = temp;
+        // Идет свап значений между собой с использованием временной переменной
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
